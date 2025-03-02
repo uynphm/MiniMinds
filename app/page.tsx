f0d2c632c711766def0056bf93335be9268ce262
Uyen Pham <uyenbao.pt@gmail.com>
2025-03-01 20:09:58 -0800

    fix welcome page button

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,27 @@ import Link from "next/link"
 import { ArrowRight } from "lucide-react"
 import NavigationBar from "@/components/navigationbar";
 import { Sparkles, Brain, Shield } from "lucide-react";
+import { Border } from "@/components/border";
 
 export default function WelcomePage() {
   return (
     <main className="flex h-screen flex-col items-center justify-center p-4 sm:p-24 bg-blue-custom text-white overflow-hidden">
-      <div className="w-full absolute inset-0 overflow-hidden">
-        <NavigationBar />
-      </div>
-      <div className="mt-8">
+      <div className="mt-8 w-full h-full justify-center items-center flex flex-col">
+        {/* Navigation bar */}
+        <div className="w-full absolute inset-0 overflow-hidden">
+          <NavigationBar />
+        </div>
         <div className="w-full max-w-4xl text-center space-y-8 relative z-10 animate-bounce-in">
-          <h1 className="text-4xl sm:text-7xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-blue-950 animate-pulse">
+          <h1 className="text-4xl sm:text-7xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-b from-blue-700 to-blue-950 animate-pulse">
             Welcome to MiniMinds
           </h1>
-          <p className="font-semibold text-2xl sm:text-2xl text-black-200 max-w-2xl mx-auto font-space-grotesk">
-            Autism is a spectrum disorder, and it affects each person differently. We are here to help detect early signs of autism in toddlers.
+          <p className="font-semibold text-xl sm:text-lg text-black-200 max-w-2xl mx-auto font-space-grotesk">
+            Autism affects every child differently, but early detection can can lead to timely intervention and better outcomes. With our app, parents can recognize early signs in toddlers and access the resources needed to support their child’s development. Start detecting early - because every step matters!
           </p>
 
           {/* Content container */}
           <div className="grid md:grid-cols-3 gap-8 mb-16">
-            <div className="bg-blue-950 rounded-xl shadow-lg p-8 transform transition-all duration-300 hover:-translate-y-5 hover:shadow-xl mb-3">
+            <div className="bg-blue-950 rounded-xl shadow-lg p-8 transform transition-all duration-300 hover:-translate-y-5 hover:shadow-xl mt-3 mb-5">
               <div className="bg-blue-100 rounded-full w-14 h-14 flex items-center justify-center mb-6">
                 <Sparkles className="h-7 w-7 text-blue-950" />
               </div>
@@ -30,7 +32,7 @@ export default function WelcomePage() {
               </p>
             </div>
 
-            <div className="bg-blue-950 rounded-xl shadow-lg p-8 transform transition-all duration-300 hover:-translate-y-5 hover:shadow-xl mb-3">
+            <div className="bg-blue-950 rounded-xl shadow-lg p-8 transform transition-all duration-300 hover:-translate-y-5 hover:shadow-xl mt-3 mb-5">
               <div className="bg-blue-100 rounded-full w-14 h-14 flex items-center justify-center mb-6">
                 <Brain className="h-7 w-7 text-blue-950" />
               </div>
@@ -40,7 +42,7 @@ export default function WelcomePage() {
               </p>
             </div>
 
-            <div className="bg-blue-950 rounded-xl shadow-lg p-8 transform transition-all duration-300 hover:-translate-y-5 hover:shadow-xl mb-3">
+            <div className="bg-blue-950 rounded-xl shadow-lg p-8 transform transition-all duration-300 hover:-translate-y-5 hover:shadow-xl mt-3 mb-5">
               <div className="bg-blue-100 rounded-full w-14 h-14 flex items-center justify-center mb-6">
                 <Shield className="h-7 w-7 text-blue-950" />
               </div>
@@ -53,10 +55,13 @@ export default function WelcomePage() {
 
           <Link
             href="/UploadPage"
-            className="inline-flex items-center px-6 pt-3 py-3 text-lg font-medium text-white bg-blue-950 rounded-full hover:bg-gray-600 transition-colors duration-300 group"
           >
-            Start Detection
-            <ArrowRight className="ml-2 h-5 w-5 group-hover:translate-x-1 transition-transform duration-300" />
+            <Border
+              className="inline-flex items-center px-4 pt-3 py-3 font-medium bg-blue-100 rounded-full transition-colors duration-300 group"
+            >
+              Start Detection
+              <ArrowRight className="ml-2 h-5 w-5 group-hover:translate-x-1 transition-transform duration-300" />
+            </Border>
           </Link>
         </div>
       </div>
